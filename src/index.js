e0760463648c1f239517bc288eb6eb16416d525f
Rebeca Serrano Ayuso <reba4@hotmail.es>
2023-07-17 11:21:03 +0200

    add fetch

--- src/index.js
+++ src/index.js
@@ -11,3 +11,23 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+//HACER ENDPOINT Y CONECTAR A LA BASE DE DATOS // EDITAR
+async function getConnection() { //esto devuelve una promesa // los await are equivalente a los "then" del fetch
+  const connection = await mysql2.createConnection({
+      host: 'sql.freedb.tech',
+      database: 'freedb_project molones',
+      user: 'freedb_Group2',
+      password: 'nnDTSJ#mTwUg?7W',
+  
+  });
+
+  await connection.connect(); 
+
+  console.log(
+    `Conexión establecida con la base de datos (identificador=${connection.threadId})`);
+
+  return connection;
+}
+
+getConnection()
